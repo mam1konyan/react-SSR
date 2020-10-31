e6acf7ad5880de74a774b331150f19920c260929
Paruyr Mamikonyan <pashfaf@gmail.com>
2020-10-31 20:37:40 +0400

    reset styled error

--- src/client/app.js
+++ src/client/app.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import Root from 'components/root'
-import ResetStyles from './reset-styles'
-import ScrollbarStyles from './scrollbar-styles'
 
 const App = () => {
     return (
         <div>
-            <ResetStyles />
-            <ScrollbarStyles />
             <Root />
         </div>
     )
