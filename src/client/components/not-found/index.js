f3d00bae016630b8a6d91be7e5f4a81389c51c3e
Paruyr Mamikonyan <pashfaf@gmail.com>
2020-11-01 17:27:29 +0400

    add NotFound component

---
+++ src/client/components/not-found/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NotFound = () => {
+    return (
+        <div>
+            Hello from NotFound. You're seen this page, because URL you type are
+            not correct!
+        </div>
+    )
+}
+
+export default NotFound
